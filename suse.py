71846fec214afa30e573f96e214774aa24118506
unknown <circicircorum@gmail.com>
2024-08-04 15:23:21 +0900

    fixed runScramble and main by moving batch solves to main.

--- suse.py
+++ suse.py
@@ -8,13 +8,16 @@ def runScramble(verbosity=2):
     # solve with verbosity level 2
     res = sinny.solver(scr_a, verbosity=verbosity) # i.e. stages = default_stages
 
-    # batch solves
-    sinny.batch(n_scrambles=3, verbosity=verbosity) # i.e. stages = default_stages
-
     return [scr_a, res]
 
 def main():
-    rs_res = runScramble() # default verbosity is 2.
+    # perform runScramble
+    verbosity = 2
+    rs_res = runScramble(verbosity) # default verbosity is 2.
+
+    # batch solves
+    sinny.batch(n_scrambles=3, verbosity=verbosity) # i.e. stages = default_stages
+
     # print(f"ret val: {rs_res}")
 
 if '__name__' == '__main__':
